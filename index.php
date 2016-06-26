cc07bf8f8b2d19bf172eed1ef00ac5286188842a
nealhartley <hartleneal@myvuw.ac.nz>
2016-06-26 17:02:24 +1200

    adding all

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<html>
+<link type="text/css" rel="stylesheet" href="style.css">
+    <div id="fullpage">
+        <div id="container">
+            <?php
+            include 'toolBar.php';
+            include 'frontPageBody.php';            
+            ?>
+            
+                       
+            
+            
+        </div>
+    </div>
+</html>
---
+++ toolBar.php
@@ -0,0 +1,36 @@
+
+
+<!--The toolbar for all pages. include at star of project for toolbar.
+make sure to link to css: style.css on your page for the right styling"-->
+<html>
+    
+    <link type="text/css" rel="stylesheet" href="style.css">
+    
+    <div id="toolbar">
+
+                
+                <!--The logo-->
+                <div id="logo">
+                    <!--Original image 650wide by 250high-->
+                    <img src="Images/LogoName.png"
+                         Width ="280"
+                         Height="100"
+                         />
+                </div>
+
+              
+                <!--let's play button-->
+                <div id="letsPlay">
+                 <a href = "https://www.facebook.com">
+                     <!--Original image 200wide by 100high-->
+                     <image src = "Images/letsPlayButtonRed.png"
+                            Width="100"
+                            Height="50"/>
+                     </a>
+                </div>
+
+        </div>
+                        
+            <!--the grey spacer bar at bottom of the toolbar-->
+            <div id="spacer"></div>
+</html>
